1a84d89a33b97f56a98a21749b2426ec85310ecb
ThiagoMartins <thiago.alvesmartins17@gmail.com>
2022-08-10 11:48:58 -0300

    commit inicial

--- src/main/java/br/com/alura/linguagensapi/LinguagemController.java
+++ src/main/java/br/com/alura/linguagensapi/LinguagemController.java
@@ -14,7 +14,7 @@ public class LinguagemController {
     @Autowired
     private LinguagemRepository repositorio;
 
-    @GetMapping(value = "/linguagens")
+    @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens(){
         List<Linguagem> linguagens = repositorio.findAll();
         return linguagens;
